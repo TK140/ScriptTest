34a0967aa714dfe5daae5781cf8701550333065a
TK140 <yukimitsu.shimakawa@qdenbs.com>
2020-01-31 17:10:23 +0900

    ScriptTestのコミット
    Lesson4課題で作成したファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// 配列（５個）を初期設定する
+		int[] array = {11, 22, 33, 44, 55};
+
+	    	// 配列の要素数の昇順に処理を繰り返す
+		    for (int i = 0; i < array.Length; i++) {
+		    	// 配列の要素を表示する
+			    Debug.Log (array [i]);
+			    }
+
+		   // 配列の要素数の降順に処理を繰り返す
+		   for (int j = array.Length - 1; j >= 0; j--) {
+			   // 配列の要素を表示する
+			   Debug.Log (array [j]);
+		       }
+	   }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
